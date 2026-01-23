8a37ea6b6294a9400dfffcaf6e61075853d246db
fox <kayraxs1453@gmail.com>
2026-01-23 12:26:57 +0300

    Aİ'ın amk
    Mal AI yerine KAYRA YILMAZ tarafından yazıldı

--- Motor-ESP32/test1/test1/test1.ino
+++ Motor-ESP32/test1/test1/test1.ino
@@ -1,38 +1,213 @@
 #include <Arduino.h>
 
-// --- MOTOR 4 PIN DEFINITIONS (Pins 7, 8, 9) ---
-const int PIN_PWM = 7;   // Speed Control (PWM)
-const int PIN_IN1 = 8;   // Direction 1
-const int PIN_IN2 = 9;   // Direction 2
+// --- MOTOR 4 PIN DEFINITIONS 
+const int RL_PIN_PWM = 7;   // Speed Control (PWM) yeşil
+const int RL_PIN_IN1 = 8;   // Direction 1.        turuncu
+const int RL_PIN_IN2 = 9;   // Direction 2.        sarı
+
+const int RR_PIN_PWM = 11;
+const int RR_PIN_IN1 = 12;
+const int RR_PIN_IN2 = 13;
+
+const int FL_PIN_PWM = 15;
+const int FL_PIN_IN1 = 16;
+const int FL_PIN_IN2 = 17;
+
+const int FR_PIN_PWM = 4;
+const int FR_PIN_IN1 = 5;
+const int FR_PIN_IN2 = 6;
 
 // Configuration
-const int PWM_MIN_SPEED = 100; // Min speed
-const int PWM_MAX_SPEED = 255; // Full speed
+const int PWM = 200; // Full speed
+const int PWMS = 0; // Full speed
 
 void setup() {
   Serial.begin(115200);
-  while (!Serial) delay(10);
+Serial.print("code by ""Dead To AI"" Community");
 
-  Serial.println("--- TB6612FNG Speed Test (Pins 7, 8, 9) ---");
-
-  pinMode(PIN_PWM, OUTPUT);
-  pinMode(PIN_IN1, OUTPUT);
-  pinMode(PIN_IN2, OUTPUT);
+  pinMode(RL_PIN_PWM, OUTPUT);
+  pinMode(RL_PIN_IN1, OUTPUT);
+  pinMode(RL_PIN_IN2, OUTPUT);
+  pinMode(RR_PIN_PWM, OUTPUT);
+  pinMode(RR_PIN_IN1, OUTPUT);
+  pinMode(RR_PIN_IN2, OUTPUT);
+  pinMode(FL_PIN_PWM, OUTPUT);
+  pinMode(FL_PIN_IN1, OUTPUT);
+  pinMode(FL_PIN_IN2, OUTPUT);
+  pinMode(FR_PIN_PWM, OUTPUT);
+  pinMode(FR_PIN_IN1, OUTPUT);
+  pinMode(FR_PIN_IN2, OUTPUT);
 }
 
 void loop() {
-  // 1. Set Direction (Forward)
-  // Important: One must be HIGH, the other LOW
-  digitalWrite(PIN_IN1, HIGH);
-  digitalWrite(PIN_IN2, LOW);
 
-  // 2. Go FULL SPEED (255) for 1 second
-  Serial.println("Speed: FULL (255)");
-  analogWrite(PIN_PWM, PWM_MAX_SPEED);
-  delay(1000);
+ileri ();
+delay(1000);
+dur ();
+delay(1000);
+geri ();
+delay(1000);
+dur ();
+delay(1000);
+sag ();
+delay(1000);
+dur ();
+delay(1000);
+sol ();
+delay(1000);
+dur ();
+delay(1000);
+sag360 ();
+delay(1000);
+dur ();
+delay(1000);
+sol360 ();
+delay(1000);
+dur ();
+delay(1000);
 
-  // 3. Go MIN SPEED (100) for 1 second
-  Serial.println("Speed: MIN (100)");
-  analogWrite(PIN_PWM, PWM_MIN_SPEED);
-  delay(1000);
+}
+
+void ileri(){
+
+  digitalWrite(RR_PIN_IN1, HIGH);
+  digitalWrite(RR_PIN_IN2, LOW);
+
+  digitalWrite(FR_PIN_IN1, HIGH);
+  digitalWrite(FR_PIN_IN2, LOW);
+
+  digitalWrite(RL_PIN_IN1, HIGH);
+  digitalWrite(RL_PIN_IN2, LOW);
+
+  digitalWrite(FL_PIN_IN1, HIGH);
+  digitalWrite(FL_PIN_IN2, LOW);
+
+
+  analogWrite(RR_PIN_PWM, PWM);
+  analogWrite(RL_PIN_PWM, PWM);
+  analogWrite(FL_PIN_PWM, PWM);
+  analogWrite(FR_PIN_PWM, PWM);
+
+}
+void geri(){
+
+  digitalWrite(RR_PIN_IN1, LOW);
+  digitalWrite(RR_PIN_IN2, HIGH);
+
+  digitalWrite(FR_PIN_IN1, LOW);
+  digitalWrite(FR_PIN_IN2, HIGH);
+
+  digitalWrite(RL_PIN_IN1, LOW);
+  digitalWrite(RL_PIN_IN2, HIGH);
+
+  digitalWrite(FL_PIN_IN1, LOW);
+  digitalWrite(FL_PIN_IN2, HIGH);
+
+
+  analogWrite(RR_PIN_PWM, PWM);
+  analogWrite(RL_PIN_PWM, PWM);
+  analogWrite(FL_PIN_PWM, PWM);
+  analogWrite(FR_PIN_PWM, PWM);
+
+}
+void sag(){
+  
+  digitalWrite(RR_PIN_IN1, HIGH);
+  digitalWrite(RR_PIN_IN2, LOW);
+
+  digitalWrite(FR_PIN_IN1, LOW);
+  digitalWrite(FR_PIN_IN2, HIGH);
+
+  digitalWrite(RL_PIN_IN1, LOW);
+  digitalWrite(RL_PIN_IN2, HIGH);
+
+  digitalWrite(FL_PIN_IN1, HIGH);
+  digitalWrite(FL_PIN_IN2, LOW);
+
+
+  analogWrite(RR_PIN_PWM, PWM);
+  analogWrite(RL_PIN_PWM, PWM);
+  analogWrite(FL_PIN_PWM, PWM);
+  analogWrite(FR_PIN_PWM, PWM);
+}
+void sol(){
+  
+  digitalWrite(RR_PIN_IN1, LOW);
+  digitalWrite(RR_PIN_IN2, HIGH);
+
+  digitalWrite(FR_PIN_IN1, HIGH);
+  digitalWrite(FR_PIN_IN2, LOW);
+
+  digitalWrite(RL_PIN_IN1, HIGH);
+  digitalWrite(RL_PIN_IN2, LOW);
+
+  digitalWrite(FL_PIN_IN1, LOW);
+  digitalWrite(FL_PIN_IN2, HIGH);
+
+
+  analogWrite(RR_PIN_PWM, PWM);
+  analogWrite(RL_PIN_PWM, PWM);
+  analogWrite(FL_PIN_PWM, PWM);
+  analogWrite(FR_PIN_PWM, PWM);
+}
+void dur(){
+  
+  digitalWrite(RR_PIN_IN1, LOW);
+  digitalWrite(RR_PIN_IN2, LOW);
+
+  digitalWrite(RL_PIN_IN1, LOW);
+  digitalWrite(RL_PIN_IN2, LOW);
+
+  digitalWrite(FL_PIN_IN1, LOW);
+  digitalWrite(FL_PIN_IN2, LOW);
+
+  digitalWrite(FR_PIN_IN1, LOW);
+  digitalWrite(FR_PIN_IN2, LOW);
+
+
+  analogWrite(RR_PIN_PWM, PWMS);
+  analogWrite(RL_PIN_PWM, PWMS);
+  analogWrite(FL_PIN_PWM, PWMS);
+  analogWrite(FR_PIN_PWM, PWMS);
+
+}
+void sol360(){
+
+  digitalWrite(RR_PIN_IN1, HIGH);
+  digitalWrite(RR_PIN_IN2, LOW);
+
+  digitalWrite(FR_PIN_IN1, HIGH);
+  digitalWrite(FR_PIN_IN2, LOW);
+
+  digitalWrite(RL_PIN_IN1, LOW);
+  digitalWrite(RL_PIN_IN2, HIGH);
+
+  digitalWrite(FL_PIN_IN1, LOW);
+  digitalWrite(FL_PIN_IN2, HIGH);
+
+
+  analogWrite(RR_PIN_PWM, PWM);
+  analogWrite(RL_PIN_PWM, PWM);
+  analogWrite(FL_PIN_PWM, PWM);
+  analogWrite(FR_PIN_PWM, PWM);
+}
+void sag360(){
+
+  digitalWrite(RR_PIN_IN1, LOW);
+  digitalWrite(RR_PIN_IN2, HIGH);
+
+  digitalWrite(FR_PIN_IN1, LOW);
+  digitalWrite(FR_PIN_IN2, HIGH);
+
+  digitalWrite(RL_PIN_IN1, HIGH);
+  digitalWrite(RL_PIN_IN2, LOW);
+
+  digitalWrite(FL_PIN_IN1, HIGH);
+  digitalWrite(FL_PIN_IN2, LOW);
+
+
+  analogWrite(RR_PIN_PWM, PWM);
+  analogWrite(RL_PIN_PWM, PWM);
+  analogWrite(FL_PIN_PWM, PWM);
+  analogWrite(FR_PIN_PWM, PWM);
 }
