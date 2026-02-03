fb0af771e8c2b2c8a660138d7ca207f11e15788d
callisto1232 <ahmet_ka1232@hotmail.com>
2026-02-03 14:27:32 +0300

    Update dene_ahmet.ino

--- PID-ESP32/PIDD/ahmet/dene_ahmet/dene_ahmet.ino
+++ PID-ESP32/PIDD/ahmet/dene_ahmet/dene_ahmet.ino
@@ -1,7 +1,14 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include <WiFiUdp.h>
-#include <ESP32Servo.h>
+
+WiFiUDP udp;
+unsigned int localPort = 4210;
+char packetBuffer[255];
+unsigned long lastUpdate = 0;
+
+#define WIFI_SSID "Fox-2"
+#define WIFI_PSWD "Kyra2bin9"
 
 // --- MOTOR PIN DEFINITIONS ---
 const int RL_PWM = 7;
@@ -32,40 +39,67 @@ const int ER_IN2 = 42;
 
 const int E_LID = 41;
 
-// Networking
-WiFiUDP udp;
-const unsigned int localPort = 4210;
-char packetBuffer[255];
-
 // Inputs
 float a0=0, a2=0, a4=0, a5=0;
 int b0=0, b6=0, b11=0, b12=0, b13=0;
 
-// Servo state
-int prev_b6 = 0;
-bool lid_open = false;
-Servo lid_servo;
 
 void setup() {
   Serial.begin(115200);
   
   // Motor pins
-  pinMode(RL_IN1, OUTPUT); pinMode(RL_IN2, OUTPUT); pinMode(RL_PWM, OUTPUT);
-  pinMode(RR_IN1, OUTPUT); pinMode(RR_IN2, OUTPUT); pinMode(RR_PWM, OUTPUT);
-  pinMode(FL_IN1, OUTPUT); pinMode(FL_IN2, OUTPUT); pinMode(FL_PWM, OUTPUT);
-  pinMode(FR_IN1, OUTPUT); pinMode(FR_IN2, OUTPUT); pinMode(FR_PWM, OUTPUT);
+  pinMode(RL_IN1, OUTPUT);
+  pinMode(RL_IN2, OUTPUT);
+  pinMode(RL_PWM, OUTPUT);
+
+  pinMode(RR_IN1, OUTPUT);
+  pinMode(RR_IN2, OUTPUT);
+  pinMode(RR_PWM, OUTPUT);
+
+  pinMode(FL_IN1, OUTPUT);
+  pinMode(FL_IN2, OUTPUT);
+  pinMode(FL_PWM, OUTPUT);
+
+  pinMode(FR_IN1, OUTPUT);
+  pinMode(FR_IN2, OUTPUT);
+  pinMode(FR_PWM, OUTPUT);
   
-  pinMode(EL_IN1, OUTPUT); pinMode(EL_IN2, OUTPUT); pinMode(EL_PWM, OUTPUT);
-  pinMode(ER_IN1, OUTPUT); pinMode(ER_IN2, OUTPUT); pinMode(ER_PWM, OUTPUT);
+  pinMode(EL_IN1, OUTPUT);
+  pinMode(EL_IN2, OUTPUT);
+  pinMode(EL_PWM, OUTPUT);
+
+  pinMode(ER_IN1, OUTPUT);
+  pinMode(ER_IN2, OUTPUT);
+  pinMode(ER_PWM, OUTPUT);
   
-  lid_servo.attach(E_LID);
   
   // WiFi
-  WiFi.begin("Fox-2", "Kyra2bin9");
+  WiFi.begin(WIFI_SSID, WIFI_PSWD);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
   Serial.println("\nWiFi OK! IP: " + WiFi.localIP().toString());
   udp.begin(localPort);
+}
+
+
+void loop(){
+  digitalWrite(RL_IN1, HIGH);
+  digitalWrite(RL_IN2, LOW);
+  analogWrite(RL_PWM, 255);
+
+  digitalWrite(RR_IN1, LOW);
+  digitalWrite(RR_IN2, HIGH);
+  analogWrite(RR_PWM, 255);
+
+  digitalWrite(FL_IN1, HIGH);
+  digitalWrite(FL_IN2, LOW);
+  analogWrite(FL_PWM, 255);
+
+  digitalWrite(FR_IN1, HIGH);
+  digitalWrite(FR_IN2, LOW);
+  analogWrite(FR_PWM, 255);
+
+
 }
