592480467e6d1596a81719605166ed031290d064
fox <kayraxs1453@gmail.com>
2026-02-06 21:52:45 +0300

    .

---
+++ LKayra/LKayra.ino
@@ -0,0 +1,96 @@
+// 192.168.4.1
+
+// MECHATAK - SIMPLE MOTOR TEST (NO ProBot complications)
+// MECHATAK - SIMPLE MOTOR TEST (NO ProBot complications)
+
+#define PROBOT_WIFI_AP_PASSWORD "kayra123"
+#define PROBOT_WIFI_AP_SSID "MECHATAK"
+#define PROBOT_WIFI_AP_CHANNEL 3
+#include <probot.h>
+
+// http://192.168.4.1
+#include <probot/devices/servo/servo.hpp>
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+void ileri(){
+
+
+}
+
+void geri(){
+
+
+}
+
+void sagslide(){
+
+
+}
+
+void solslide(){
+
+
+
+}
+
+void sol360(){
+
+
+}
+
+void sag360(){
+
+
+}
+
+void dur(){
+
+
+}
+
+void anidur(){
+
+
+}
+
+void sagon(){
+
+
+}
+
+void solon(){
+
+
+}
+
+void sagarka(){
+
+
+}
+
+void solarka(){
+
+
+}
+
+void eup(){
+
+
+}
+
+void edown(){
+
+
+}
+
