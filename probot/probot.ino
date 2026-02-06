09ce983ee245dde08a0cfa401fd42bef85830e5c
fox <kayraxs1453@gmail.com>
2026-02-06 14:10:06 +0300

    Reapply "."
    This reverts commit bae79c6e5a95e63c0d6d4d1cc5c4e4d9d4e4a3bd.

--- probot/probot.ino
+++ probot/probot.ino
@@ -19,18 +19,33 @@ const int EL_PWM = 18; const int EL_IN1 = 38; const int EL_IN2 = 39;
 const int E_LID = 41;
 const int B_LID = 14;
 
+<<<<<<< Updated upstream
+=======
+
+// Mecanum surus icin dort motorun pin atamalari.
+>>>>>>> Stashed changes
 struct MotorPins {
   int ina, inb, pwm, ena, enb;
 };
+
 
 static constexpr MotorPins PINS_FL{FL_IN1, FL_IN2, FL_PWM, -1, -1};
 static constexpr MotorPins PINS_FR{FR_IN1, FR_IN2, FR_PWM, -1, -1};
 static constexpr MotorPins PINS_RL{RL_IN1, RL_IN2, RL_PWM, -1, -1};
 static constexpr MotorPins PINS_RR{RR_IN1, RR_IN2, RR_PWM, -1, -1};
 
+<<<<<<< Updated upstream
 // Elevator motor controller
 static constexpr MotorPins PINS_EL{EL_IN1, EL_IN2, EL_PWM, -1, -1};
 static probot::motor::BoardozaVNH5019MotorController drvEL(PINS_EL.ina, PINS_EL.inb, PINS_EL.pwm, PINS_EL.ena, PINS_EL.enb);
+=======
+
+static probot::motor::BoardozaVNH5019MotorController drvFL(PINS_FL.ina, PINS_FL.inb, PINS_FL.pwm, PINS_FL.ena, PINS_FL.enb);
+static probot::motor::BoardozaVNH5019MotorController drvFR(PINS_FR.ina, PINS_FR.inb, PINS_FR.pwm, PINS_FR.ena, PINS_FR.enb);
+static probot::motor::BoardozaVNH5019MotorController drvRL(PINS_RL.ina, PINS_RL.inb, PINS_RL.pwm, PINS_RL.ena, PINS_RL.enb);
+static probot::motor::BoardozaVNH5019MotorController drvRR(PINS_RR.ina, PINS_RR.inb, PINS_RR.pwm, PINS_RR.ena, PINS_RR.enb);
+>>>>>>> Stashed changes
+
 
 static probot::command::examples::MecanumDrive mecanum(&drvFL, &drvFR, &drvRL, &drvRR);
 using Scheduler = probot::command::Scheduler;
